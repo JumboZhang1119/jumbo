baf2c5e2202529a4be456a56769c54b30a6251b9
PO-FENG CHANG <107752584+JumboZhang1119@users.noreply.github.com>
2025-06-21 00:16:07 +0800

    Create route.tsx

---
+++ src/app/blog/sitemap.xml/route.tsx
@@ -0,0 +1,24 @@
+import { config } from "@/config";
+import { wisp } from "@/lib/wisp";
+import type { MetadataRoute } from "next";
+import urlJoin from "url-join";
+
+export const revalidate = 0;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const result = await wisp.getPosts();
+  return [
+    {
+      url: urlJoin(config.baseUrl, "blog"),
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    ...result.posts.map((post) => {
+      return {
+        url: urlJoin(config.baseUrl, "blog", post.slug),
+        lastModified: new Date(post.updatedAt),
+        priority: 0.8,
+      };
+    }),
+  ];
+}
