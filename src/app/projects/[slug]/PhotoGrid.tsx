ad73d63ea699deea4fb26d7c19ebed1602e36942
PO-FENG CHANG <zhangbofeng150916@gmail.com>
2025-06-24 19:31:30 +0800

    Update about page content for clarity and adjust PhotoGrid layout for better responsiveness

--- src/app/projects/[slug]/PhotoGrid.tsx
+++ src/app/projects/[slug]/PhotoGrid.tsx
@@ -279,7 +279,7 @@ export default function PhotoGrid({ photos }: PhotoGridProps) {
                 </div>
                 
                 <div className="w-full flex justify-center mt-1 mb-4">
-                  <div className="w-full max-w-[670px] flex justify-between gap-x-[1px] bg-black/60 text-white text-[10px] sm:text-xs px-3 py-1 rounded-md">
+                  <div className="w-full max-w-[700px] flex justify-between gap-x-[1px] bg-black/60 text-white text-[10px] sm:text-xs px-3 py-1 rounded-md">
                     <div className="flex-1 truncate text-left">
                       Camera: {selectedPhoto.content.cameraModel}
                     </div>
